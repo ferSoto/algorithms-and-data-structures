f2a860118c6e1ced36e87762b33869b06ef4d5aa
Fernando de Jesús Soto <fersoto117@gmail.com>
2017-03-20 01:05:51 -0500

    Find all triangles

--- Triangles/main.cpp
+++ Triangles/main.cpp
@@ -13,7 +13,10 @@
 *****/
 
 
+#include <algorithm>
 #include <iostream>
+#include <set>
+#include <tuple>
 #include <vector>
 
 
@@ -23,8 +26,13 @@ using namespace std;
 /***** TYPES ****/
 
 typedef unsigned int uint;
-typedef vector<bool> BooleanMatrix;
-typedef vector<vector<uint>> IntegerMatrix;
+typedef vector<uint> UintVector;
+typedef vector<UintVector> IntegerMatrix;
+
+typedef vector<bool> BooleanVector;
+
+typedef tuple<uint, uint, uint> Triangle;
+typedef set<Triangle> TriangleSet;
 
 /****************/
 
@@ -34,14 +42,20 @@ typedef vector<vector<uint>> IntegerMatrix;
 uint verticesNumber;
 IntegerMatrix graph;
 
+TriangleSet triangleSet;
+BooleanVector visited;
+
+UintVector path(3);
+
 /******************/
 
-IntegerMatrix transformToIntegerMatrix(BooleanMatrix booleanMatrix, uint verticesNumber) {
+
+IntegerMatrix transformToIntegerMatrix(BooleanVector booleanVector, uint verticesNumber) {
     IntegerMatrix matrix(verticesNumber);
 
     for (uint i = 0; i < verticesNumber; i++) {
         for (uint j = 0; j < verticesNumber; j++) {
-            if (booleanMatrix[i * verticesNumber + j]) {
+            if (booleanVector[i * verticesNumber + j]) {
                 matrix[i].push_back(j);
             }
         }
@@ -50,21 +64,73 @@ IntegerMatrix transformToIntegerMatrix(BooleanMatrix booleanMatrix, uint vertice
     return matrix;
 }
 
+void saveTriangle() {
+    UintVector sortedPath(path);
+    sort(sortedPath.begin(), sortedPath.end());
+
+    triangleSet.insert((Triangle){sortedPath[0], sortedPath[1], sortedPath[2]});
+}
+
+bool isNeighbor(uint vertex, uint neighbor) {
+    for (auto i : graph[vertex]) {
+        if (i == neighbor) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void dfs(uint vertex, uint level, uint initialVertex) {
+    visited[vertex] = true;
+    path[level] = vertex;
+
+    if (level == 2) {
+        if (isNeighbor(vertex, initialVertex)) saveTriangle();
+
+    } else {
+        for (auto n : graph[vertex]) {
+            if (!visited[n]) dfs(n, level + 1, initialVertex);
+        }
+    }
+
+    visited[vertex] = false;
+}
+
+void findTrianglesForVertex(uint vertex) {
+    dfs(vertex, 0, vertex);
+}
+
+void printTriangle(Triangle triangle) {
+    cout << get<0>(triangle) << " -> ";
+    cout << get<1>(triangle) << " -> ";
+    cout << get<2>(triangle) << endl;
+}
+
 
 int main() {
     cin >> verticesNumber;
 
     uint matrixLength = verticesNumber * verticesNumber;
-    BooleanMatrix booleanMatrix(matrixLength);
+    BooleanVector booleanVector(matrixLength);
 
     for (uint i = 0; i < matrixLength; i++) {
         bool boolValue;
         cin >> boolValue;
-        booleanMatrix[i] = boolValue;
+        booleanVector[i] = boolValue;
     }
 
-    graph = transformToIntegerMatrix(booleanMatrix, verticesNumber);
+    graph = transformToIntegerMatrix(booleanVector, verticesNumber);
 
+    // Find triangles
+    visited.resize(verticesNumber);
+    for (uint i = 0; i < verticesNumber; i++) {
+        findTrianglesForVertex(i);
+    }
+
+    for (auto triangle : triangleSet) {
+        printTriangle(triangle);
+    }
 
     return 0;
 }
