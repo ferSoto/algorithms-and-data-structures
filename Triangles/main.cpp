89bc2f57aac7feb43be417dfd1985db8d47f9dfd
Fernando de Jesús Soto <fersoto117@gmail.com>
2017-03-19 20:37:14 -0500

    Create source code file

---
+++ Triangles/main.cpp
@@ -0,0 +1,22 @@
+/****
+*
+*  Find all the triangles of a graph using DFS.
+*
+*****
+*
+*  Authors:
+*    Cynthia Alexis Allende Pascual
+*    Javier Alonso Chávez Hernández
+*    Jorge Carlos González González
+*    Fernando de Jesús Soto
+*
+*****/
+
+
+using namespace std;
+
+
+int main() {
+
+    return 0;
+}
