cc1742d9c8ce1862b7697c916102403277d269fa
Fernando de Jesús Soto <fersoto117@gmail.com>
2017-03-19 21:40:28 -0500

    Create integer matrix
    - Also, add test file

--- Triangles/main.cpp
+++ Triangles/main.cpp
@@ -13,10 +13,58 @@
 *****/
 
 
+#include <iostream>
+#include <vector>
+
+
 using namespace std;
 
 
+/***** TYPES ****/
+
+typedef unsigned int uint;
+typedef vector<bool> BooleanMatrix;
+typedef vector<vector<uint>> IntegerMatrix;
+
+/****************/
+
+
+/***** GLOBAL *****/
+
+uint verticesNumber;
+IntegerMatrix graph;
+
+/******************/
+
+IntegerMatrix transformToIntegerMatrix(BooleanMatrix booleanMatrix, uint verticesNumber) {
+    IntegerMatrix matrix(verticesNumber);
+
+    for (uint i = 0; i < verticesNumber; i++) {
+        for (uint j = 0; j < verticesNumber; j++) {
+            if (booleanMatrix[i * verticesNumber + j]) {
+                matrix[i].push_back(j);
+            }
+        }
+    }
+
+    return matrix;
+}
+
+
 int main() {
+    cin >> verticesNumber;
+
+    uint matrixLength = verticesNumber * verticesNumber;
+    BooleanMatrix booleanMatrix(matrixLength);
+
+    for (uint i = 0; i < matrixLength; i++) {
+        bool boolValue;
+        cin >> boolValue;
+        booleanMatrix[i] = boolValue;
+    }
+
+    graph = transformToIntegerMatrix(booleanMatrix, verticesNumber);
+
 
     return 0;
 }
