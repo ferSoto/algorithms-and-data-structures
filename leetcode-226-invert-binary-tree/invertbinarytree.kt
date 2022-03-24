ae64e777edf3e1fa50460e86679fb22ccba8c1c2
Fernando Soto <fersoto117@gmail.com>
2022-03-23 23:36:27 -0700

    Inverti binary tree

---
+++ leetcode-226-invert-binary-tree/invertbinarytree.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        return root?.apply {
+            val temp = left
+            left = invertTree(right)
+            right = invertTree(temp)
+        }
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+}
